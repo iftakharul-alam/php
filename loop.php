45ae73b444735d9ac85718f1ee97c1a838e6f434
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-09 15:33:11 +0600

    loop v.1

--- loop.php
+++ loop.php
@@ -1,10 +1,13 @@
 <?php
-    $start = 1;
-    $end = 100;
-    $summation = 0;
+    //example of for loop
+    $start = 1;                     //initial variable
+    $end = 100;                     //end variable
+    $summation = 0;                 //Summation variable
     while($start <= $end)
     {
         $summation += $start;
-        $start++;
+        $start++;                   // start variable increment by 1
     }
-    echo "Summation is $summation";
+    echo "Summation is $summation"; //output the result
+    
+    echo "<br/>------------------------<br/>";
