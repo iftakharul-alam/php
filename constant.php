73a3ff671694e1dcff35eab842ae4acfaff5d754
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-09 13:07:01 +0600

    constant and array

---
+++ constant.php
@@ -0,0 +1,10 @@
+<?php
+    //constant
+    define('PI',3.14);
+    echo PI;
+    echo "<br/>";
+    //Array
+    $cars = array('toyota','ferari','audi');
+    echo gettype($cars);
+    echo "<br/>";
+    print_r($cars);//for printing array use print_r
