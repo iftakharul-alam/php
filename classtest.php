2f60b3c1727d6fe1a6088b2556b714c269460200
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 11:26:01 +0600

    ctass test add separately

--- classtest.php
+++ classtest.php
@@ -1,86 +1,11 @@
 <?php
-    echo "<h1>";
-    function getGrade1($marks)
-    {
-        if($marks>69)
-        {
-            $grade='A+';
-        }
-        else if($marks>29)
-        {
-            $grade='A';
-        }
-        else
-        {
-            $grade='F';
-        }
-        return $grade;
-    }
-    echo "The grade is now    ".getGrade1(95); 
-    
-    echo "<br/>----------------------------------------<br/>";
-    
-    function printGrade($marks)
-    {
-       if($marks > 50)
-       {
-            echo 'A';  
-       }
-       else
-       {
-            echo 'F';
-       }
-       
-       return;
-    }
-    
-    printGrade(90);
-    
-    echo "<br/>----------------------------------------<br/>";
-    
-    function getGrade($marks = '')
-    {
-        if($marks>100)
-        {
-            return;
-        }
-        
-       $grade = '';
-       
-       if($marks > 50)
-       {
-            $grade = 'A';  
-       }
-       else
-       {
-            $grade = 'F';
-       }
-       
-       return $grade;
-    }
-    
-    
-    function echoGrade($marks)
-    {
-        $grade = getGrade($marks);
-        echo $grade;
-    }
-    
-    echoGrade(40);
-    
-    echo "<br/>----------------------------------------<br/>";
-    
-    
-    
-    
-    
     
     function filmSummary($type)
     {   
         echo "<h1><u> Film Genres : $type<br/></u><h2>";
         $film = array(
             'genres' => array('comedy','tragedy','action','romance'),
-            'film_title' => array('Big','Star Wars','Titnic','French Kiss'),
+            'film_title' => array('BIG','STAR WARS','TITANIC','FRENCH KISS'),
             'stars' => array('Bill Murry','Mark Hammel','Leonardo Decaprio','Cate Blanchell'),
         );
         if($type == $film['genres'][0])
