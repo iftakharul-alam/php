f5893dac730f08672794c4b47eafebae1adbf782
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 13:24:30 +0600

    method in parameter

--- oop.php
+++ oop.php
@@ -1,20 +1,30 @@
 <?php
     class User             //declare a class
     {
-        var $name = 'go';
-        var $email = 22.45;
+        var $name = '';
+        var $email = '';
+        
+        function __construct($name='FTFL',$email='go$go.com') 
+        {
+            $this->name = $name;
+            $this->email = $email;
+        }
         
         function getName()
         {
-           return 'FTFL2'; 
+           return $this->name; 
         }
     }
     
     $user = new User();    //instantiate a object
+    $user1 = new User('to','00');
+    $user2 = new User();
     
-    var_dump($user);        //debug the user object
+    //var_dump($user);        //debug the user object
     
-    echo $user->name;     //accessing a property of a object
+    //echo $user->name;     //accessing a property of a object
     
-    echo $user->getName();
+    echo $user1->getName(); //accessing a method of a object
+    echo "<br/>------------------------------------<br/>";
+    echo $user2->getName();
     
