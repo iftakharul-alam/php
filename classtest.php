7f551cd6e9c1f983592cbecc981cc2d7d25a34eb
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 11:56:30 +0600

    ctass test final

--- classtest.php
+++ classtest.php
@@ -4,41 +4,41 @@
     {   
         echo "<h1><u> Film Genres : $type<br/></u><h2>";
         $film = array(
-            'genres' => array('comedy','tragedy','action','romance'),
+            'genres' => array('Comedy','Tragedy','Action','Romance'),
             'film_title' => array('BIG','STAR WARS','TITANIC','FRENCH KISS'),
             'stars' => array('Bill Murry','Mark Hammel','Leonardo Decaprio','Cate Blanchell'),
         );
         if($type == $film['genres'][0])
         {
-            echo 'Film Title : '.$film['film_title'][0].' ('.  strtolower($film['film_title'][0]).')<br/>';
-            echo 'Stars : '.$film['stars'][0].' ('. str_replace(' ','-',$film['stars'][0]).')<br/>';
+            echo 'Film Title : '.$film['film_title'][0].' ('.  str_replace(' ','-',strtolower($film['film_title'][0])).')<br/>';
+            echo 'Stars : '.$film['stars'][0].' ('. str_replace(' ','-',  strtolower($film['stars'][0])).')<br/>';
             
         }
         
         else if($type == $film['genres'][1])
         {
-            echo 'Film Title : '.$film['film_title'][1].' ('.  strtolower($film['film_title'][1]).')<br/>';
-            echo 'Stars : '.$film['stars'][1].' ('.  str_replace(' ','-',$film['stars'][1]).')<br/>';
+            echo 'Film Title : '.$film['film_title'][1].' ('.  str_replace(' ','-',strtolower($film['film_title'][1])).')<br/>';
+            echo 'Stars : '.$film['stars'][1].' ('.  str_replace(' ','-',strtolower($film['stars'][1])).')<br/>';
             
         }
         
         else if($type == $film['genres'][2])
         {
-            echo 'Film Title : '.$film['film_title'][2].' ('.  strtolower($film['film_title'][2]).')<br/>';
-            echo 'Stars : '.$film['stars'][2].' ('.  str_replace(' ','-',$film['stars'][2]).')<br/>';
+            echo 'Film Title : '.$film['film_title'][2].' ('.  str_replace(' ','-',strtolower($film['film_title'][2])).')<br/>';
+            echo 'Stars : '.$film['stars'][2].' ('.  str_replace(' ','-',strtolower($film['stars'][2])).')<br/>';
             
         }
         
         else if($type == $film['genres'][3])
         {
-            echo 'Film Title : '.$film['film_title'][3].' ('.  strtolower($film['film_title'][3]).')<br/>';
-            echo 'Stars : '.$film['stars'][3].' ('.  str_replace(' ','-',$film['stars'][3]).')<br/>';
+            echo 'Film Title : '.$film['film_title'][3].' ('.  str_replace(' ','-',strtolower($film['film_title'][3])).')<br/>';
+            echo 'Stars : '.$film['stars'][3].' ('.  str_replace(' ','-',strtolower($film['stars'][3])).')<br/>';
             
         }
         
     }
     
-    filmSummary('tragedy');
+    filmSummary('Action');
     
     
     
