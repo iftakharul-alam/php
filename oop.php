21891abb4b58c2816a8b5e4ad9fe097441961fe8
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 12:40:52 +0600

    accessing the property

--- oop.php
+++ oop.php
@@ -1,9 +1,19 @@
 <?php
     class User             //declare a class
     {
+        var $name = 'go';
+        var $email = 22.45;
         
+        function getName()
+        {
+            
+        }
     }
     
     $user = new User();    //instantiate a object
     
     var_dump($user);        //debug the user object
+    
+    echo $user->name;       //accessing a property of a object
+    
+    
