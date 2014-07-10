f0a5077775f0bd504e61aa92df4520be765b76c3
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 11:04:14 +0600

    function array test v.1

--- function.php
+++ function.php
@@ -70,7 +70,41 @@
     
     echo "<br/>----------------------------------------<br/>";
     
+    $film = array(
+            'genres' => array('comedy','tragedy','action','romance'),
+            'film_title' => array('Big','Star Wars','Titnic','French Kiss'),
+            'stars' => array('Bill Murry','Mark Hammel','Leonardo Decaprio','Cate Blanchell'),
+        );
     
+    
+    
+    
+    
+    function filmSummary($type)
+    {   
+        $film = array(
+            'genres' => array('comedy','tragedy','action','romance'),
+            'film_title' => array('Big','Star Wars','Titnic','French Kiss'),
+            'stars' => array('Bill Murry','Mark Hammel','Leonardo Decaprio','Cate Blanchell'),
+        );
+        if($type == $film['genres'][0])
+        {
+            echo 'Film Title : '.$film['film_title'][0].'<br/>';
+            echo 'Stars : '.$film['stars'][0].'<br/>';
+            echo 'Slug : '.strtolower($film['stars'][0]).'<br/>';
+        }
+        
+        else if($type == $film['tragedy'][0])
+        {
+            echo 'Film Title : '.$film['film_title'][0].'<br/>';
+            echo 'Stars : '.$film['stars'][0].'<br/>';
+            echo 'Slug : '.strtolower($film['stars'][0]).'<br/>';
+        }
+        
+        
+    }
+    
+    filmSummary('tragedy');
     
     
     
