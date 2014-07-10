fe0262d90e7719595b99f3f25493799292b8b1a0
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 08:53:56 +0600

    added array

--- function.php
+++ function.php
@@ -16,4 +16,51 @@
         }
         return $grade;
     }
-    echo "The grade is now ".getGrade(95);    
+    echo "The grade is now    ".getGrade(95);   
+    //---------------------------------------------
+    echo '<br/>----------------------------------<br/>';
+    echo "<pre>";
+    
+    $cars = array(
+                array('one','two','three','four'),
+                array('1  ','2  ','3    ')
+            );
+    
+    $laravel = array('I','II','III','IV');
+    
+    $cars[2][] = $laravel;
+    
+    print_r ($cars);
+    
+    echo '<br/>----------------------------------<br/>';
+    
+    $cars = array('one','two','three','four');
+    $fruits = array(1,2,3,4);
+    $laravel = array('I','II','III','IV');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
