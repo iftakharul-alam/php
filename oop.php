5e606eaf2ed00e58c82f7c792d95596099cfd49d
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 15:02:40 +0600

    use of identifier

--- oop.php
+++ oop.php
@@ -1,5 +1,5 @@
 <?php
-    class User             //declare a class
+    class Useri             //declare a class
     {
         var $name = '';
         var $email = '';
@@ -14,17 +14,79 @@
         {
            return $this->name; 
         }
+        
+        function getEmail()
+        {
+            return $this->email;
+        }
     }
     
-    $user = new User();    //instantiate a object
-    $user1 = new User('to','00');
-    $user2 = new User();
+    //$user = new User();    //instantiate a object
+    $user1 = new Useri();
+    $user2 = new Useri();
     
     //var_dump($user);        //debug the user object
     
     //echo $user->name;     //accessing a property of a object
     
     echo $user1->getName(); //accessing a method of a object
-    echo "<br/>------------------------------------<br/>";
-    echo $user2->getName();
+    echo "<br/>";
+    echo $user1->getEmail();
+    
+    echo "<br/>------------------------------------------------------<br/>";
+    
+    class User
+    {
+        protected function userAge()
+        {
+            return 'some value';
+        }
+        
+    }
+    
+    class Person extends User
+    {
+        public function getUserAge()
+        {
+            return $this->userAge();
+        }
+    }
+    
+    $user = new Person();
+    
+    echo $user->getUserAge();
+    
+    echo $user->userAge();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
