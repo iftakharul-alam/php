6d656edeffa70d4a325ebd272c98bb33d4a224ed
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-10 11:37:18 +0600

    ctass test v.2

--- classtest.php
+++ classtest.php
@@ -11,34 +11,34 @@
         if($type == $film['genres'][0])
         {
             echo 'Film Title : '.$film['film_title'][0].' ('.  strtolower($film['film_title'][0]).')<br/>';
-            echo 'Stars : '.$film['stars'][0].' ('.  strtolower($film['stars'][0]).')<br/>';
+            echo 'Stars : '.$film['stars'][0].' ('. str_replace(' ','-',$film['stars'][0]).')<br/>';
             
         }
         
         else if($type == $film['genres'][1])
         {
             echo 'Film Title : '.$film['film_title'][1].' ('.  strtolower($film['film_title'][1]).')<br/>';
-            echo 'Stars : '.$film['stars'][1].' ('.  strtolower($film['stars'][1]).')<br/>';
+            echo 'Stars : '.$film['stars'][1].' ('.  str_replace(' ','-',$film['stars'][1]).')<br/>';
             
         }
         
         else if($type == $film['genres'][2])
         {
             echo 'Film Title : '.$film['film_title'][2].' ('.  strtolower($film['film_title'][2]).')<br/>';
-            echo 'Stars : '.$film['stars'][2].' ('.  strtolower($film['stars'][2]).')<br/>';
+            echo 'Stars : '.$film['stars'][2].' ('.  str_replace(' ','-',$film['stars'][2]).')<br/>';
             
         }
         
         else if($type == $film['genres'][3])
         {
             echo 'Film Title : '.$film['film_title'][3].' ('.  strtolower($film['film_title'][3]).')<br/>';
-            echo 'Stars : '.$film['stars'][3].' ('.  strtolower($film['stars'][3]).')<br/>';
+            echo 'Stars : '.$film['stars'][3].' ('.  str_replace(' ','-',$film['stars'][3]).')<br/>';
             
         }
         
     }
     
-    filmSummary('action');
+    filmSummary('tragedy');
     
     
     
