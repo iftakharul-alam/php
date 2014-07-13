36711e2f2b132f886652216a91ce606cdef487a8
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-12 19:32:27 -0800

    Array function array chunk

--- arrayTest.php
+++ arrayTest.php
@@ -1,7 +1,10 @@
 <?php
+	echo "<pre>";
+
 	$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
 	print_r(array_change_key_case($age,CASE_UPPER));
 	
 	echo "<br/>";
 	
-	
+	$cars=array("Volvo","BMW","Toyota","Honda","Mercedes","Opel");
+	print_r(array_chunk($cars,2));
