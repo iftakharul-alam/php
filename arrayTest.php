3b2de8bae7885bdfe5038c94578983bc67000f49
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-12 20:21:02 -0800

    Array function array list

--- arrayTest.php
+++ arrayTest.php
@@ -148,6 +148,12 @@
 	$people = array("Peter", "Joe", "Glenn", "Cleveland");
 	print_r (each($people));
 	
+	echo "<br/>";
+	
+	$my_array = array("Dog","Cat","Horse");
+	
+	list($a, $b, $c) = $my_array;
+	echo "I have several animals, a $a, a $b and a $c.";
 	
 	
 	
