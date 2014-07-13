90c912f2cc52c8e371610833cbea8f39b383ef51
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-13 12:37:14 +0600

    simple interest calculate

--- officeemployee.php
+++ officeemployee.php
@@ -7,22 +7,22 @@
                 
                 public function get_salary_with_bonus()
                 {
-                    if($this->salary<=10000)
+                    if($this -> salary <= 10000)
                     {   
-                        $bonus  = $this->salary * .5;
-                        return $this->salary + $bonus;
+                        $bonus  = $this -> salary * .5;
+                        return $this -> salary + $bonus;
                     }
                     
-                    else if($this->salary>=10000 && $this->salary<=30000)
+                    else if($this -> salary >= 10000 && $this -> salary <= 30000)
                     {   
-                        $bonus  = $this->salary * .3;
-                        return $this->salary + $bonus;
+                        $bonus  = $this -> salary * .3;
+                        return $this -> salary + $bonus;
                     }
                     
-                    else if($this->salary>=30000)
+                    else if($this -> salary >= 30000)
                     {   
-                        $bonus  = $this->salary * .2;
-                        return $this->salary + $bonus;
+                        $bonus  = $this -> salary * .2;
+                        return $this -> salary + $bonus;
                     }
                     
                     
