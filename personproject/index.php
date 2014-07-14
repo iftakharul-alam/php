362b03d89ebab53d3b4adb943899e07e2b64d5b9
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-14 12:21:19 +0600

    using setter getter

--- personproject/index.php
+++ personproject/index.php
@@ -6,9 +6,10 @@
         <?php
             require 'person.php';
             $a_person = new Person();
-            $a_person -> first_name = 'one';
-            $a_person -> last_name = 'two';
-            $a_person -> middle_name = 'three';
+            //$a_person -> first_name = 'one';
+            $a_person -> set_first_name('James');
+            $a_person -> set_middle_name('Row');
+            $a_person -> set_last_name('Wimbley');
 
             echo $a_person -> get_full_name()."<br/>";
             echo $a_person -> get_reverse_name();
--- personproject/person.php
+++ personproject/person.php
@@ -1,9 +1,31 @@
 <?php
     class Person
     {
-        public $first_name;
-        public $middle_name;
-        public $last_name;
+        private $first_name;
+        private $middle_name;
+        private $last_name;
+
+        public function set_first_name($first_name)
+        {   if(strlen($first_name) >= 2)
+            {
+                $this -> first_name = $first_name;
+            }
+        }
+
+        public function get_first_name()
+        {
+            $this -> first_name;
+        }
+
+        public function set_middle_name($middle_name)
+        {
+            $this -> middle_name = $middle_name;
+        }
+
+        public function set_last_name($last_name)
+        {
+            $this -> last_name = $last_name;
+        }
 
         public function get_full_name()
         {
