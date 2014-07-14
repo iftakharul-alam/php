4915d6bc7cf45e380a135c01f1c6e6fabdbfb01a
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-14 12:48:08 +0600

    using __constructor

--- personproject/index.php
+++ personproject/index.php
@@ -5,11 +5,11 @@
     <body>
         <?php
             require 'person.php';
-            $a_person = new Person();
+            $a_person = new Person('James','R.','Gavin');
             //$a_person -> first_name = 'one';
-            $a_person -> set_first_name('James');
-            $a_person -> set_middle_name('Row');
-            $a_person -> set_last_name('Wimbley');
+            //$a_person -> setFirstName('James');
+            //$a_person -> setMiddleName('W');
+            //$a_person -> setLastName('Moddy');
 
             echo $a_person -> get_full_name()."<br/>";
             echo $a_person -> get_reverse_name();
--- personproject/person.php
+++ personproject/person.php
@@ -5,27 +5,53 @@
         private $middle_name;
         private $last_name;
 
-        public function set_first_name($first_name)
-        {   if(strlen($first_name) >= 2)
-            {
-                $this -> first_name = $first_name;
-            }
+        function __construct($middle_name, $last_name, $first_name)
+        {
+            $this -> setFirstName($first_name); //call this method for logic
+
+            $this->middle_name = $middle_name;
+            $this->last_name = $last_name;
+            $this->first_name = $first_name;
         }
 
-        public function get_first_name()
+
+        public function setFirstName($first_name)
         {
-            $this -> first_name;
+            $this->first_name = $first_name;
         }
 
-        public function set_middle_name($middle_name)
+
+        public function getFirstName()
         {
-            $this -> middle_name = $middle_name;
+            return $this->first_name;
         }
 
-        public function set_last_name($last_name)
+
+        public function setMiddleName($middle_name)
         {
-            $this -> last_name = $last_name;
+            $this->middle_name = $middle_name;
         }
+
+
+        public function getMiddleName()
+        {
+            return $this->middle_name;
+        }
+
+
+
+        public function setLastName($last_name)
+        {
+            $this->last_name = $last_name;
+        }
+
+
+        public function getLastName()
+        {
+            return $this->last_name;
+        }
+
+
 
         public function get_full_name()
         {
