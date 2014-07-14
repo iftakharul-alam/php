8a8c52a3b32e8e9bde0c938a94b58b70dbc820ce
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-14 12:59:55 +0600

    session started

---
+++ session/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Session page
+        </title>
+    </head>
+    <body>
+    <?php
+        $a_number = 100;
+        session_start();
+        $a_number = $_SESSION['number1'];
+        $_SESSION['number1'] = $_SESSION['number1'] + 10;
+        echo $_SESSION['number1'];
+    ?>
+    </body>
+</html>
